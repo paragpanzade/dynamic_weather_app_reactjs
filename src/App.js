2e62e4b8e5c014dde977f09e8d3cde1a004f4555
Parag Panzade <panzadeparag@gmail.com>
2023-02-06 20:34:37 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/assets/cold.jpg 	new file:   src/assets/hot.jpg 	new file:   src/components/description.css 	new file:   src/components/description.jsx 	modified:   src/index.css 	new file:   src/weather_service.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import coldbg from "./assets/cold.jpg"
+import hotbg from "./assets/hot.jpg"
+import "./App.css";
+import Description from "./components/description";
+import { useEffect, useState } from "react";
+import { getFormattedWeatherData} from './weather_service';
 
 function App() {
+
+  const [city, setCity] = useState('Paris');
+  const [weather ,setWeather] = useState(null);
+  const [units, setUnits] = useState("metric");
+  const [bg, setBg] = useState(hotbg);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const data = await getFormattedWeatherData(city, units);
+      setWeather(data);
+
+      // dynamic bg
+      const threshold = units === "metric" ? 20 : 68;
+      if (data.temp <= threshold) setBg(coldbg);
+      else setBg(hotbg);
+    };
+
+    fetchWeatherData();
+  }, [units, city]);
+
+  const handleUnitsClick = (e) => {
+    const button = e.currentTarget;
+    const currentUnit = button.innerText.slice(1);
+
+    const isCelsius = currentUnit === "C";
+    button.innerText = isCelsius ? "°F" : "°C";
+    setUnits(isCelsius ? "metric" : "imperial");
+  };
+  const enterKeyPressed = (e) => {
+    if (e.keyCode === 13) {
+      setCity(e.currentTarget.value);
+      e.currentTarget.blur();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><div className="App" style={{ backgroundImage: `url(${bg})`}}>
+      {
+        weather && (
+      <div className="container">
+      <br></br><br></br>
+        <input onKeyDown={enterKeyPressed} className="cityname" type="text" name="city" placeholder='Enter City Name'></input>
+        <button className="button" onClick={(e) => handleUnitsClick(e)}>°F</button>
+        <br></br><br></br>
+        <center>
+        <div className="section_temperature">
+          <p className="alignLeft">
+            <h3> { `${weather.name}, ${weather.country}`}</h3>
+              <img src={weather.iconURL} alt="weatherIcon" height='80' width='80'></img>
+              <h3>{weather.description}</h3>
+          </p>
+          <p className="alignRight">
+            <h1>{`${weather.temp.toFixed()} °${ units === "metric" ? "C" : "F"} `}</h1>
+          </p>
+        </div>
+        </center>
+        
+        <Description weather={weather} units={units}>
+
+        </Description>
+      </div>
+        )
+      }
+    </div></>
+    
   );
 }
 
